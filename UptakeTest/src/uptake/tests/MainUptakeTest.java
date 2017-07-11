360acec1dcd3f0d32b286e4140ac069c8d99fdb7
= <=>
2017-07-11 07:18:45 -0500

    Recommit priorities addition

--- UptakeTest/src/uptake/tests/MainUptakeTest.java
+++ UptakeTest/src/uptake/tests/MainUptakeTest.java
@@ -3,7 +3,6 @@ package uptake.tests;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import uptake.automation.*;
@@ -20,16 +19,20 @@ public class MainUptakeTest {
 	}
 	
 	
-	@Test
-	public void PagelaunchTest(){
+	@Test (priority=1)
+	public void PagelaunchTest() {
 		String url = "https://www.uptake.com/";
+		
 		Assert.assertEquals(driver.getUrl(), url);
+
 	}
 	
-	@Test
+	@Test  (priority=2)
 	//@Parameters("linkText")
 	public void PageLinkTest(){
-		driver.navigateToURL();		
+		
+		driver.navigateToURL();	
+		
 		driver.getUrl();
 		Assert.assertEquals(driver.getUrl(), "https://www.uptake.com/approach");
 		
